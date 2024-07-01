890d282305e4d284db57b48d8f81bcebd768d000
Habeeb Rahman <habeeb.rp@2basetechnologies.com>
2024-07-01 22:20:04 +0530

    UI Updates.

--- lib/core/theme/app_themes.dart
+++ lib/core/theme/app_themes.dart
@@ -5,24 +5,32 @@ import 'package:flutter/services.dart';
 class AppThemes {
   static ThemeData get light {
     return ThemeData(
-      brightness: Brightness.light,
-      // scaffoldBackgroundColor: Colors.white,
-      fontFamily: 'SegoeUI',
-      appBarTheme: AppBarTheme(
-        backgroundColor: AppColors.appColor,
-        foregroundColor: AppColors.white,
-        surfaceTintColor: AppColors.white,
-        titleTextStyle: const TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.w600,
+        brightness: Brightness.light,
+        // scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'SegoeUI',
+        appBarTheme: AppBarTheme(
+          backgroundColor: AppColors.appColor,
+          foregroundColor: AppColors.white,
+          surfaceTintColor: AppColors.white,
+          titleTextStyle: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+          systemOverlayStyle: SystemUiOverlayStyle.dark.copyWith(
+            statusBarColor: AppColors.appColor,
+            statusBarIconBrightness: Brightness.light,
+            systemNavigationBarColor: AppColors.white,
+            systemNavigationBarIconBrightness: Brightness.dark,
+          ),
         ),
-        systemOverlayStyle: SystemUiOverlayStyle.dark.copyWith(
-          statusBarColor: AppColors.appColor,
-          statusBarIconBrightness: Brightness.light,
-          systemNavigationBarColor: AppColors.white,
-          systemNavigationBarIconBrightness: Brightness.dark,
+        bottomSheetTheme: const BottomSheetThemeData(
+          backgroundColor: AppColors.white,
         ),
-      ),
-    );
+        datePickerTheme: const DatePickerThemeData(
+          backgroundColor: AppColors.white,
+        ),
+        dialogTheme: const DialogTheme(
+          backgroundColor: AppColors.white,
+        ));
   }
 }
